61f017c048f5ef2996d8df03b0e9a97eb0698bcd
Mateus Fonseca <mateusfsantos.pfs@gmail.com>
2024-09-10 14:20:54 -0300

    - Feat: Create Goal Route

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,11 +1,22 @@
 import fastify from "fastify";
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from "fastify-type-provider-zod";
+import { createGoalRoute } from "./routes/create-goal";
 
-const app = fastify();
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.register(createGoalRoute);
 
 app
   .listen({
     port: 3333,
   })
   .then(() => {
-    console.log("aoba");
+    console.log("HTTP server running!");
   });
